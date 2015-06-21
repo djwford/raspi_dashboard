6f568c972658f93e797820f4b106969e3d2de541
Derek <derekweatherford@gmail.com>
2015-06-21 13:38:21 -0600

    fixes temp record controller.

--- app/controllers/temp_records_controller.rb
+++ app/controllers/temp_records_controller.rb
@@ -7,7 +7,9 @@ class TempRecordsController < ApplicationController
       format.json {
         @tempRecord = TempRecord.new(temp_record_params)
         if TempRecord.any?
-          if @tempRecord.record_date.to_date.day == ((TempRecord.last.record_date.to_date + 1.day).day)
+          # check if the record is for the day after the last record's record_date
+          if @tempRecord.record_date.to_date.day >= ((TempRecord.last.record_date.to_date + 1.day).day)
+            # create a new record
             puts "record_date"
             puts @tempRecord.record_date
             puts "last record_date"
